f1ac0545134fbfa86d089994cc3512ca21d529ec
Henrico Gonçalves <henrico_mg@hotmail.com>
2025-02-09 16:38:42 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿static void Menu(){
+    Console.Clear();
+    Console.WriteLine("O que você deseja fazer?");
+    Console.WriteLine("1 - Abrir Arquivo");
+    Console.WriteLine("2 - Criar novo arquivo");
+    Console.WriteLine("3 - Sair");
+    short option = short.Parse(Console.ReadLine());
+
+    switch(option){
+        
+    }
+}
